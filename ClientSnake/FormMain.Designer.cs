a545a982a5b6e054e91142e6a38c3119d3e1043c
Roman <LarscoRS@yandex.ru>
2016-05-05 11:46:28 +0300

    Add Server

---
+++ ClientSnake/FormMain.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ClientSnake
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxMes = new System.Windows.Forms.TextBox();
+            this.richTextBoxChat = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonSearchGame = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(203, 6);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(84, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Соединиться";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(12, 9);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(185, 20);
+            this.textBoxName.TabIndex = 1;
+            this.textBoxName.Text = "UserName";
+            this.textBoxName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxName_KeyDown);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Location = new System.Drawing.Point(12, 177);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 3;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxMes
+            // 
+            this.textBoxMes.Location = new System.Drawing.Point(93, 177);
+            this.textBoxMes.Name = "textBoxMes";
+            this.textBoxMes.Size = new System.Drawing.Size(193, 20);
+            this.textBoxMes.TabIndex = 4;
+            this.textBoxMes.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxMes_KeyDown);
+            // 
+            // richTextBoxChat
+            // 
+            this.richTextBoxChat.Location = new System.Drawing.Point(12, 36);
+            this.richTextBoxChat.Name = "richTextBoxChat";
+            this.richTextBoxChat.Size = new System.Drawing.Size(275, 132);
+            this.richTextBoxChat.TabIndex = 5;
+            this.richTextBoxChat.Text = "";
+            this.richTextBoxChat.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBoxChat_KeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.GreenYellow;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(293, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(515, 515);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonSearchGame
+            // 
+            this.buttonSearchGame.Enabled = false;
+            this.buttonSearchGame.Location = new System.Drawing.Point(12, 206);
+            this.buttonSearchGame.Name = "buttonSearchGame";
+            this.buttonSearchGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearchGame.TabIndex = 7;
+            this.buttonSearchGame.Text = "Найти игру";
+            this.buttonSearchGame.UseVisualStyleBackColor = true;
+            this.buttonSearchGame.Click += new System.EventHandler(this.buttonSearchGame_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 539);
+            this.Controls.Add(this.buttonSearchGame);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxMes);
+            this.Controls.Add(this.richTextBoxChat);
+            this.Name = "FormMain";
+            this.Text = "Client";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxMes;
+        private System.Windows.Forms.RichTextBox richTextBoxChat;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonSearchGame;
+    }
+}
+
---
+++ ClientSnake/FormMain.cs
@@ -0,0 +1,229 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNet.SignalR.Client;
+
+using System.Net;
+using System.Net.Http;
+using System.Windows.Forms;
+using SnakeLib;
+namespace ClientSnake
+{
+    public partial class FormMain : Form
+    {
+        int W = 51, H = 51, S = 10;
+        int X = 0, Y = 0;
+        int way = 0; // направление движения змеи: 0 - вверх, 1 - вправо, 2 - вниз, 3 - влево
+       
+        Map map;
+        List<Snake> snakes;
+        Food Food;
+
+
+        public FormMain()
+        {
+            InitializeComponent();
+            map = new Map(W,H);
+      
+            this.pictureBox1.Paint += new PaintEventHandler(Program_Paint);
+   
+            this.pictureBox1.Focus();
+            this.pictureBox1.PreviewKeyDown += PictureBox1_PreviewKeyDown;
+
+           
+        }
+
+        private void PictureBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            Action act = () => richTextBoxChat.AppendText("Сообщение knopka:" + Environment.NewLine);
+            richTextBoxChat.Invoke(act);
+        }
+
+        void Program_Paint(object sender, PaintEventArgs e)
+        {
+            if (snakes != null)
+            {
+                for (var j = 0; j < snakes.Count; j++)
+                {
+                    if (snakes[j].Coordinates.Count>0)
+                    {
+                        e.Graphics.FillRectangle(new SolidBrush(snakes[j].Head), new Rectangle(snakes[j].Coordinates[0].X * S, snakes[j].Coordinates[0].Y * S, S, S));
+                    }
+                    for (var i = 1; i < snakes[j].Coordinates.Count; i++)
+                    {
+                        e.Graphics.FillRectangle(new SolidBrush(snakes[j].Body), new Rectangle(snakes[j].Coordinates[i].X * S, snakes[j].Coordinates[i].Y * S, S, S));
+                    }
+                }              
+                e.Graphics.FillEllipse(new SolidBrush(Food.Color), new Rectangle(Food.Coordinate.X * S, Food.Coordinate.Y * S, S, S));
+            
+              
+                e.Graphics.FillRectangle(new SolidBrush(Color.Brown), new Rectangle(0 * S, 0 * S, H*S, S));
+                e.Graphics.FillRectangle(new SolidBrush(Color.Brown), new Rectangle(0 * S, 0 * S, S, W*S));
+                e.Graphics.FillRectangle(new SolidBrush(Color.Brown), new Rectangle((W-1) * S, 0 * S, (H-1) * S, (W - 1) * S));
+                e.Graphics.FillRectangle(new SolidBrush(Color.Brown), new Rectangle(0 * S, (H-1) * S, H  * S, W*S));
+            
+            }
+    }
+
+        void Program_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Connection != null)
+            {
+                
+                switch (e.KeyData)
+                {
+                    case Keys.Up:
+                        if (way != 2)
+                        {
+                            way = 0;
+                            SendWay(way);
+                        }
+                        break;
+                    case Keys.Right:
+                        if (way != 3)
+                        {
+                            way = 1;
+                            SendWay(way);
+                        }
+                        break;
+                    case Keys.Down:
+                        if (way != 0)
+                        {
+                            way = 2;
+                            SendWay(way);
+                        }
+                        break;
+                    case Keys.Left:
+                        if (way != 1)
+                        {
+                            way = 3;
+                            SendWay(way);
+                        }
+                        break;
+
+                }
+               
+            }
+        }
+
+        
+
+        public IHubProxy HubProxy { set; get; }
+        //const string ServerURI = "http://spimex24.ru:55555/signalr";
+        //const string ServerURI = "http://192.168.1.140:55555/signalr";
+        const string ServerURI = "http://localhost:55555/signalr";
+        public HubConnection Connection { set; get; }
+
+    
+        public void ConnectAsync()
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("Name", textBoxName.Text);
+            
+            Connection = new HubConnection(ServerURI, dict);
+            HubProxy = Connection.CreateHubProxy("SnakeHub");
+
+            HubProxy.On<string>("NewMessage", (mes) =>
+            {
+                  Action act = () => richTextBoxChat.AppendText("Сообщение получено:" + mes+Environment.NewLine);
+                  richTextBoxChat.Invoke(act);                       
+            }
+            );
+
+            HubProxy.On<List<Snake>, Food>("GameTick", (r, food) =>
+            {
+                snakes = r;
+                Food = food;
+                this.pictureBox1.Invalidate();
+               
+            }
+            );
+
+            HubProxy.On<int, int>("AppleTick", (x, y) =>
+            {
+                Action act = () =>
+                {
+                    X = x;
+                    Y = y;
+                    richTextBoxChat.AppendText("apple1:" + x.ToString() + " " + y.ToString() + Environment.NewLine);
+                    this.pictureBox1.Invalidate();
+                };
+                richTextBoxChat.Invoke(act);
+            }
+            );
+
+            HubProxy.On("Off", () =>
+            {
+                Connection.Stop();
+               // Connection.Dispose();
+            }
+          );
+
+            try
+            {
+                Connection.Start();
+               
+            }
+            catch(HttpRequestException)
+            {
+
+            }
+        }
+
+        private void textBoxName_KeyDown(object sender, KeyEventArgs e)
+        {
+            Program_KeyDown(sender, e);
+        }
+
+        private void textBoxMes_KeyDown(object sender, KeyEventArgs e)
+        {
+            Program_KeyDown(sender, e);
+        }
+
+        private void richTextBoxChat_KeyDown(object sender, KeyEventArgs e)
+        {
+            Program_KeyDown(sender, e);
+        }
+
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(Connection!=null)
+                Connection.Stop();
+        }
+
+        private void buttonSearchGame_Click(object sender, EventArgs e)
+        {
+            HubProxy.Invoke("SearchGame");
+            buttonSearchGame.Enabled = false;
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            ConnectAsync();
+            buttonConnect.Enabled = false;
+            buttonSend.Enabled = true;
+            buttonSearchGame.Enabled = true;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            HubProxy.Invoke("SendMessage", textBoxMes.Text);         
+        }
+
+        private void SendWay(int way)
+        {
+            HubProxy.Invoke("SendWay", way);
+        }
+
+       
+        
+    }
+
+}
